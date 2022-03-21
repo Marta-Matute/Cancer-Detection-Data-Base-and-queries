82eb11c96cf0124f4db9564cf80ff742d0bbd54e
Marta Matute <57796819+Marta-Matute@users.noreply.github.com>
2022-03-21 18:36:35 +0100

    Added EXPERIMENT AND USERS

--- data_reading.py
+++ data_reading.py
@@ -1,3 +1,4 @@
+# METHOD
 Method = {}
 with open('Dades.xlsx’, mode='rb') as fname:
     dfe = pd.read_excel(fname, sheet_name='MethodOutput')
@@ -29,3 +30,45 @@ for rowid in range(len(dfe)):
                          "MalignPrec": row["MalignRec"]
                          }
         method[my_id]["Experiment"].append(new_dict)
+
+	  
+# EXPERIMENT
+with open('Dades.xlsx’, mode='rb') as fname: 
+	dfe = pd.read_excel(fname, sheet_name='MethodOutput') 
+
+for rowid in range(len(dfe)): 
+    row = dfe.iloc[rowid] 
+    my_id = row["Experiment"] 
+    if my_id not in my_collection: 
+	my_collection[my_id] = { 
+                         "Repetition": row["Repetition"], 
+                         "Train": row["Train"], 
+                         "BenignPrec": row["BenignPrec"] 
+                         "BenignRec": row["BenignRec"] 
+                         "MalignRec": row["MalignPrec"] 
+                         "MalignPrec": row["MalignRec"] 
+                         } 
+	  
+# USERS
+Users={} 
+with open('Dades.xlsx', mode='rb') as fname: 
+    dfe = pd.read_excel(fname, sheet_name='Cases') 
+
+for rowid in range(len(dfe)): 
+    row = dfe.iloc[rowid] 
+    my_id = row["PatientID"] 
+    if my_id not in users: 
+        users[my_id] = { 
+                "PatientID": row["PatientID"], 
+                "Age": int(row['Age']), 
+                "Gender": row["Gender"], 
+                "DiagnosisPatient": row["DiagnosisPatient"], 
+                "Nodules": [{"NoduleID": row["NoduleID"], 
+                            "DiagnosisNodul":  row["DiagnosisNodul"] 
+                            }] 
+                } 
+
+    else: 
+        new_dict = {'NoduleID': row["NoduleID"],  
+        "DiagnosisNodul": row["DiagnosisNodul"]} 
+        users[my_id]["Nodules"].append(new_dict) 
