11beebe4716632d83fedbd2fd28e5b77a7fd045f
Juditu <57796689+Juditu@users.noreply.github.com>
2022-03-21 19:29:03 +0100

    Update data_reading.py

--- data_reading.py
+++ data_reading.py
@@ -73,3 +73,38 @@ for rowid in range(len(dfe)):
         new_dict = {'NoduleID': row["NoduleID"],  
         "DiagnosisNodul": row["DiagnosisNodul"]} 
         users[my_id]["Nodules"].append(new_dict) 
+
+#NODULES
+def search(dfe, my_id, patient_id, name1, name2):
+    for i in range(len(dfe)):
+        if(dfe.iloc[i][name1]==my_id and dfe.iloc[i][name2]==patient_id):
+            return i
+    return -1
+
+nodules={}
+with open('Dades.xlsx', mode='rb') as fname:
+    dfe = pd.read_excel(fname, sheet_name='Cases')
+    dfe2 = pd.read_excel(fname, sheet_name='Training')
+
+for rowid in range(len(dfe)):
+    row = dfe.iloc[rowid]
+    #valor_del_camp = row["nom_camp"]
+    my_id = row["NoduleID"]
+    patient_id = row["PatientID"]
+    my_id2 = search(dfe2, my_id, patient_id, 'NodulID', 'PatientID')
+    row2 = dfe2.iloc[my_id2]
+   
+    if (my_id or my_id2) not in nodules:
+        nodules[my_id2] = {
+                'PatientID': row['PatientID'],
+                "NoduleID": row["NoduleID"],
+                "DiagnosisNodul": row['DiagnosisNodul'],
+                'Position': {'X': row['PositionX'], 'Y': row['PositionY'], 'Z': row['PositionZ']},
+                'CtScanner': {'CTID': row['CTID'], 'Diammeter': row['Diameter (mm)']},
+                'Experiment': {'MethodID': row2['MethodID'],
+                               'ExperimentRepetition': row2['ExperimentRepetition'],
+                               'RadiomicsDiagnosis': row2['RadiomicsDiagnosis'],
+                               'Train': row2['Train']}
+		}
+    
+
